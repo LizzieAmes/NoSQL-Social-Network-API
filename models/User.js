a5d76827c53c26f4cbf2537f5a0bf2d5c9df6975
Lizzie <lizziejacoboo@gmail.com>
2024-03-09 12:48:47 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  }, {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  });
+  userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+  const User = model('User', UserSchema);
+  
+  module.exports = assignmentSchema;
